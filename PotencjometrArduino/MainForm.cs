7808b2d9071bbaeec3d3ab85930ec5980d66cf96
Chudy-C <m.chudzinski94@gmail.com>
2021-04-01 03:52:55 +0200

    .

--- PotencjometrArduino/MainForm.cs
+++ PotencjometrArduino/MainForm.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 
 namespace PotencjometrArduino
 {
@@ -17,18 +19,17 @@ namespace PotencjometrArduino
         bool isConnected = false;
         bool updateData = false;
         String[] ports;
-        SerialPort port;
-        bool isValueReturned = false;
-        string returnedValue = "";
-        bool isFormClosed;
 
+        string fileDate;
+        uint probka;
         string percentage, volts, byteValue;
         double voltageValue;
+        int percentageInt;
 
-        private Stopwatch stopWatch;
-
-        //int milis, seconds, minutes = 0;
+        string timeStamp, milis, seconds, minutes;
         TimeSpan ts;
+        Stopwatch stopWatch;
+
         public MainForm()
         {
             InitializeComponent();
@@ -42,29 +43,36 @@ namespace PotencjometrArduino
                     comBox.SelectedItem = ports[0];
                 }
             }
+            string day = DateTime.Now.Day.ToString("00");
+            string month = DateTime.Now.Month.ToString("00");
+            string year = DateTime.Now.Year.ToString("00");
+            string hour = DateTime.Now.Hour.ToString("00");
+            string minute = DateTime.Now.Minute.ToString("00");
+            fileDate = day + "-" + month + "-" + year + "_" +hour+minute;
+            probka = 0;
+
+            stopWatch = new Stopwatch();
+
         }
 
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             progressChart.ChartAreas[0].AxisY.Minimum = 0;
-            progressChart.ChartAreas[0].AxisY.Maximum = 5;
-            progressChart.Series[0].Points.AddY(3);
+            progressChart.ChartAreas[0].AxisY.Maximum = 2;
             progressChart.Series[0].IsValueShownAsLabel = false;
             progressChart.ChartAreas[0].AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
 
             progressChart.Series[0].BorderWidth = 3;
             progressChart.Series[0].Color = Color.IndianRed;
 
-            stopWatch = new Stopwatch();
-
-            startButton.Enabled = false;
-            stopButton.Enabled = false;
-            resetTimerButton.Enabled = false;
-            patientBox.Enabled = false;
 
         }
 
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (arduinoPort.IsOpen) arduinoPort.Close();
+        }
 
         private void getAvailablePorts()
         {
@@ -81,44 +89,36 @@ namespace PotencjometrArduino
         {
             isConnected = true;
             string selectedPort = comBox.GetItemText(comBox.SelectedItem);
-            port = new SerialPort(selectedPort, 9600);
-            port.Open();
-            port.Write("Start");
+            arduinoPort = new SerialPort(selectedPort, 9600);
+            arduinoPort.Open();
+            arduinoPort.Write("Start");
+
+            arduinoPort.DataReceived += port_DataReceived;
 
             connectionText.Text = "CONNECTED";
             connectionText.ForeColor = Color.LimeGreen;
             connectionText.Visible = true;
 
             connectButton.Text = "Disconnect";
-
-            patientBox.Enabled = true;
-
+            updateData = true;
             disableControls();
+
+           //arduinoTimer.Interval = 100;
+            //arduinoTimer.Start();
         }
 
         private void disconnectFromArduino()
         {
+            //arduinoTimer.Stop();
+
             isConnected = false;
-            port.Write("STOP");
-            port.Close();
+            arduinoPort.Write("STOP");
+            arduinoPort.Close();
             connectionText.Text = "DISCONNECTED";
             connectionText.ForeColor = Color.IndianRed;
 
             connectButton.Text = "Connect";
 
-            patientBox.SelectedIndex = 0;
-            patientBox.Enabled = false;
-
-            milisecondsText.Text = "00";
-            secondsText.Text = "00";
-            minutesText.Text = "00";
-
-            startButton.Enabled = false;
-            stopButton.Enabled = false;
-            resetTimerButton.Enabled = false;
-
-            patientBox.SelectedIndex = -1;
-            patientBox.Enabled = false;
 
             lByteValue.Text = "Potentiometer Value [Byte] : 000";
             lVoltageValue.Text = "Potentiometer Value[V] : 000";
@@ -137,134 +137,103 @@ namespace PotencjometrArduino
             comBox.Enabled = false;
             progressBar.Enabled = false;
             progressBar.Value = 0;
-
-            milisecondsText.Text = "00";
-            secondsText.Text = "00";
-            minutesText.Text = "00";
-
-            startButton.Enabled = false;
         }
 
-        private void startButton_Click(object sender, EventArgs e)
+
+        private string StringToCSV(string input)
         {
-            startButton.Enabled = false;
-            stopButton.Enabled = true;
-            patientBox.Enabled = false;
+            probka++;
 
-            port.Write("START\n");
-            isValueReturned = false;
-
-            while (isValueReturned == false || isFormClosed == false)
-            {
-                if (returnedValue != "SENDING\n")
-                {
-                    returnedValue = port.ReadLine();
-                }
-                else
-                {
-                    port.WriteLine("OK");
-                    stopWatch.Start();
-                    measurementTimer.Start();
-                    isValueReturned = true;
-                }
-            }
+            input = input.Remove(0,1);
+            input = input.Replace('N', ';');
+            input = input.Replace('B', ';');
+            input = input.Replace('V', ';');
+            input = input.Replace('P', ';');
+            input = input + timeStamp;
+            return input;
         }
-
-        private void stopButton_Click(object sender, EventArgs e)
-        {
-            measurementTimer.Stop();
-            stopWatch.Stop();
-            resetTimerButton.Enabled = true;
-            stopButton.Enabled = false;
-        }
-
-        private void resetTimerButton_Click(object sender, EventArgs e)
-        {
-/*            milis = 0;
-            seconds = 0;
-            minutes = 0;*/
-            milisecondsText.Text = "00";
-            secondsText.Text = "00";
-            minutesText.Text = "00";
-
-            startButton.Enabled = true;
-            patientBox.Enabled = true;
-            resetTimerButton.Enabled = false;
-
-            stopWatch.Reset();
-        }
-
-        private void measurementTimer_Tick(object sender, EventArgs e)
-        {
-
-            ts = stopWatch.Elapsed;
-            if(ts.Milliseconds.ToString().Length == 1)
-            {
-                milisecondsText.Text = "0"+(ts.Milliseconds / 10).ToString();
-            }
-            else milisecondsText.Text = (ts.Milliseconds/10).ToString();
-            if(ts.Seconds.ToString().Length == 1)
-            {
-                secondsText.Text = "0"+ts.Seconds.ToString();
-            }
-            else secondsText.Text = ts.Seconds.ToString();
-            if (ts.Minutes.ToString().Length ==1)
-            {
-                minutesText.Text = "0"+ ts.Minutes.ToString();
-            }
-            else minutesText.Text = ts.Minutes.ToString();
-        }
-
-        private void patientBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            startButton.Enabled = true;
-
-        }
-
 
         private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            string dataIn = port.ReadTo("\n");
+            string dataIn = arduinoPort.ReadTo("\n");
             Data_Parsing(dataIn);
             this.BeginInvoke(new EventHandler(Show_data));
+
+            string path = String.Format(@"D:\5_STUDIA\inzynierka\C#_program\TxtFiles\{0}.txt", fileDate);
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    tw.WriteLine("Numer próbki;Byte;Voltage;Percentage");
+                    tw.WriteLine(StringToCSV(dataIn));
+                }
+
+            }
+            else if (File.Exists(path))
+            {
+                using (TextWriter tw = File.AppendText(path))
+                {
+                    tw.WriteLine(StringToCSV(dataIn));
+                }
+            }
         }
 
         private void Show_data(object sender, EventArgs e)
         {
-            if (updateData)
-            {
-                lByteValue.Text = string.Format("Potentiometer Value [Byte] : {0}", byteValue);
-                lVoltageValue.Text = string.Format("Potentiometer Value[V] :  {0}", volts);
-                lPercentage.Text = float.Parse(percentage).ToString();
-                progressBar.Value = Int32.Parse(percentage);
+            /* todo : Timer label 
+                - Dodać label do wyświetlania timeStamp
+                - bool sprawdzający czy Timer zaczął działać
+                    = true jeżeli przyjął dane typu: @201N1024B0.48V100P
+                    else = false;
+                - jeżeli dataReciver otrzyma "KONIEC" -> wyłącz timer oraz StopWatch
+             */
 
+            lByteValue.Text = string.Format("Potentiometer Value [Byte] : {0}", byteValue);
+            lVoltageValue.Text = string.Format("Potentiometer Value[V] :  {0}", volts);
+            lPercentage.Text = percentage;
+            progressBar.Value = percentageInt; 
 
-                progressChart.Series[0].Points.Add(voltageValue);
-                progressChart.ChartAreas[0].AxisY.Maximum = 5;
-                if (progressChart.Series[0].Points.Count == 20) progressChart.Series[0].Points.RemoveAt(0);
-            }
+            progressChart.Series[0].Points.Add(voltageValue);
+            progressChart.ChartAreas[0].AxisY.Maximum = 2;
+            if (progressChart.Series[0].Points.Count == 20) progressChart.Series[0].Points.RemoveAt(0);
         }
 
         private void Data_Parsing(string inputData)
         {
-            sbyte indexOf_startDataCharacter = (sbyte)inputData.IndexOf("@");
-            sbyte indexOfB = (sbyte)inputData.IndexOf("B");
-            sbyte indexOfV = (sbyte)inputData.IndexOf("V");
-            sbyte indexOfp = (sbyte)inputData.IndexOf("p");
 
-            if (indexOf_startDataCharacter != -1 && indexOfB != -1 && indexOfV != -1 && indexOfp != -1)
+            //@201N1024B0.48V100P
+            sbyte indexOf_startDataCharacter = (sbyte)inputData.IndexOf('@');
+            sbyte indexOfN = (sbyte)inputData.IndexOf('N');
+            sbyte indexOfB = (sbyte)inputData.IndexOf('B');
+            sbyte indexOfV = (sbyte)inputData.IndexOf('V');
+            sbyte indexOfP = (sbyte)inputData.IndexOf('P');
+
+            if (indexOf_startDataCharacter != -1 && indexOfN != -1 && indexOfB != -1 && indexOfV != -1 && indexOfP != -1)
             {
                 try
                 {
-                    string str_byte = inputData.Substring(indexOf_startDataCharacter + 1, (indexOfB - indexOf_startDataCharacter) - 1);
+                    string str_probka = inputData.Substring(indexOf_startDataCharacter + 1, (indexOfN - indexOf_startDataCharacter) - 1);
+                    string str_byte = inputData.Substring(indexOfN + 1, (indexOfB - indexOfN) - 1);
                     string str_volts = inputData.Substring(indexOfB + 1, (indexOfV - indexOfB) - 1);
-                    string str_p = inputData.Substring(indexOfV + 1, (indexOfp - indexOfV) - 1);
+                    string str_p = inputData.Substring(indexOfV + 1, (indexOfP - indexOfV) - 1);
 
                     percentage = str_p;
                     volts = str_volts;
                     byteValue = str_byte;
 
-                    voltageValue = Convert.ToDouble(str_volts);
+                    /*int.TryParse(percentage, out percentageInt);*/
+
+                    float percentageFloat = float.Parse(percentage, CultureInfo.InvariantCulture.NumberFormat);
+
+                    percentageInt = (int)Math.Round(percentageFloat);
+                    if (percentageInt > 100) percentageInt = 100;
+                    if (percentageInt < 0) percentageInt = 0;
+
+                    percentage = percentageInt.ToString();
+
+                    voltageValue = double.Parse(str_volts, System.Globalization.CultureInfo.InvariantCulture);
 
                     updateData = true;
                 }
@@ -276,23 +245,33 @@ namespace PotencjometrArduino
             else updateData = false;
         }
 
+        private void textBoxReceiver_TextChanged(object sender, EventArgs e)
+        {
+            textBoxReceiver.SelectionStart = textBoxReceiver.Text.Length;
+            textBoxReceiver.ScrollToCaret();
+        }
 
+        private void measurementTimer_Tick(object sender, EventArgs e)
+        {
+            ts = stopWatch.Elapsed;
+            if (ts.Milliseconds.ToString().Length == 1)
+            {
+                milis = "0" + (ts.Milliseconds / 10).ToString();
+            }
+            else milis = (ts.Milliseconds / 10).ToString();
+            if (ts.Seconds.ToString().Length == 1)
+            {
+                seconds = "0" + ts.Seconds.ToString();
+            }
+            else seconds = ts.Seconds.ToString();
+            if (ts.Minutes.ToString().Length == 1)
+            {
+                minutes = "0" + ts.Minutes.ToString();
+            }
+            else minutes = ts.Minutes.ToString();
 
-        /*        private void timer1_Tick(object sender, EventArgs e)
-                {
-                    var serialRead = port.ReadLine();
-                    labelPotentiometer.Text = "Potentiometer Value [Byte] : " + serialRead;
-                    var i = Convert.ToByte(serialRead);
-                    var voltageValueDouble = Convert.ToInt32(i) * 0.0196;
-                    var voltageValue = Convert.ToInt32(i);
-                    progressBar.Value = voltageValue;
-
-                    labelPotentiometerVoltage.Text = "Potentiometer Value [V] : " + Math.Round(voltageValueDouble,2).ToString();
-
-                    progressChart.Series[0].Points.Add(voltageValueDouble);
-                    progressChart.ChartAreas[0].AxisY.Maximum = 5;
-                    if (progressChart.Series[0].Points.Count == 20) progressChart.Series[0].Points.RemoveAt(0);
-                }*/
+            timeStamp = minutes+":"+seconds+"."+milis;
+        }
 
     }
 }
